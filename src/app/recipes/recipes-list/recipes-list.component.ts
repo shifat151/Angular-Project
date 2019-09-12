616e37e8e8e9b9622d33ee940e3b4002de5e7d56
Md Lutfor Rahman <shifat151@gmail.com>
2019-09-12 20:30:55 +0600

    Project Added

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+
+  recipes:Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test',
+    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg' ),
+    new Recipe('A test Recipe', 'This is simply a test',
+    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg' )
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

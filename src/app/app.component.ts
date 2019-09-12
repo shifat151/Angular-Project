19321f89b2a5ce015c0ae54284dfe043dac9894d
Md Lutfor Rahman <shifat151@gmail.com>
2019-09-12 21:38:40 +0600

    loop through navigation options
    Design updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Project';
+  loadedFeature= 'Recipe';
+
+  onNavigate(feature:string){
+    this.loadedFeature=feature;
+
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() featureSelected=new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(feature:string){
+    this.featureSelected.emit(feature);
   }
 
 }
